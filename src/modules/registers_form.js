e21fbf86a84dcee261d4d878cae8fa02abae19c3
kuzmichit <kuzmichit@gmail.com>
2023-08-28 13:55:13 +0200

    Fixed emulator, Inizio implementare service

--- src/modules/registers_form.js
+++ src/modules/registers_form.js
@@ -20,7 +20,7 @@ export async function btnRegisterFormHandler(currentDate, evt) {
 
   const dataForSaveInDatabase = new CreateObjectForDatabase(dateFormatted, dataForm);
   
-  if(!checkFillField(dataForm) ) {
+  if(!checkFillField(dataForm) ) { // controllo riempimento dei campi
     return; 
   }
 
@@ -32,9 +32,8 @@ export async function btnRegisterFormHandler(currentDate, evt) {
   }; 
 
   try{
-    const idToken = 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjYzODBlZjEyZjk1ZjkxNmNhZDdhNGNlMzg4ZDJjMmMzYzIzMDJmZGUiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vbGEtc2NlZGEtZGktbGF2b3JvIiwiYXVkIjoibGEtc2NlZGEtZGktbGF2b3JvIiwiYXV0aF90aW1lIjoxNjkyMzYxNzc3LCJ1c2VyX2lkIjoibU0wOVRTbHlEbWI4RVZ4ZEx0OGJKOXlsQzF5MSIsInN1YiI6Im1NMDlUU2x5RG1iOEVWeGRMdDhiSjl5bEMxeTEiLCJpYXQiOjE2OTIzNjE3NzcsImV4cCI6MTY5MjM2NTM3NywiZW1haWwiOiJ6dWNjYUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZW1haWwiOlsienVjY2FAZ21haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoicGFzc3dvcmQifX0.I_52VS0YonhiHLfMRdbXNgZ8nS68NJSbSLzApr3Chus0Bb6DpN_RsBIo0q5Il_pSlX1b_ofa9mgFEj5xCFWvwFeX-Z4kjxPOqMhYzVKjNnxhPSzwG15IG1tm-70lCV4MJx3mnK2oa6kVJGVrT7-oBA5jLqEPr3LECUIlQCkrVMVGno8EuVNGeI5wMhJDWO-F_ysokzzL-R3ihHlBmB-gKoac2izWan0Z-waPaArGyjGfEZSG_61vwOMaryeDqjppKx4eJRR20NZJP_B2gcTkGEsTEA1bFFECFqzvOceaok3z1lknSv0mmPdjyPW4rWV8eC5u8G9Hu29oNL2sIyRK0w';
-
-    // const idToken = await authWithEmailAndPassword(userData);
+    
+    const idToken = await authWithEmailAndPassword(userData);
   
     const currentData = await getScheduleFromDatabase(idToken, currentMonth)
     //controllo se si puo memorizzare la scheda
@@ -61,7 +60,7 @@ function CreateObjectForDatabase(date, {building, description, workedHours} ) {
 function authWithEmailAndPassword(userData) {
   const apiKey = 'AIzaSyDMLR1XYP9NpvZbXZbBxBLEWB1Ssx528ms';
 
-  return fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`, {
+  return fetch(`http://localhost:9099/identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`, {
     method: 'POST',
     mode: 'cors',
     body: JSON.stringify( {
@@ -94,7 +93,7 @@ function authWithEmailAndPassword(userData) {
 }
 
 const submitScheduleInDatabase = (dataForSaveInDatabase, dateFormatted, currentMonth, idToken, workForm) => {
-  fetch(`https://la-sceda-di-lavoro-default-rtdb.firebaseio.com/rapportinoBorys/${currentMonth}.json?auth=${idToken}`,
+  fetch(`http://127.0.0.1:9000/rapportino-service/${currentMonth}.json?auth=${idToken}`,
     {
       method: 'PATCH',
       body: JSON.stringify(dataForSaveInDatabase),
@@ -110,6 +109,7 @@ const submitScheduleInDatabase = (dataForSaveInDatabase, dateFormatted, currentM
       }
       showReport(dateFormatted, workForm);
     } )
+
     .catch( (e) => errorMessage(ConfirmBox, {messageBody: e = 'Qualcosa non va, riprova più tardi' } ) );
 }; 
 
@@ -122,7 +122,7 @@ function saveDataInLocalStorage(data, dateFormatted) {
 
 function getScheduleFromDatabase(idToken, currentMonth) {
 
-  return fetch(`https://la-sceda-di-lavoro-default-rtdb.firebaseio.com/rapportinoBorys/${currentMonth}.json?auth=${idToken}`)
+  return fetch(`http://127.0.0.1:9000/rapportino-service/${currentMonth}.json?auth=${idToken}`)
     .then(response => response.json() )
     .catch(error => alert(error.message) );
 }
